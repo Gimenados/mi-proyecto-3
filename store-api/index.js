8c8b16a1a9d24252464183b59e9f19fe113cb546
gimenados <gimenasande1@gmail.com>
2024-05-17 16:28:39 -0300

    rutas

--- store-api/index.js
+++ store-api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from "cors";
+import path from 'path'
+import { fileURLToPath } from 'url';
 import dotenv from "dotenv";
 
 import productsRoutes from "./src/routes/products.routes.js"; 
@@ -12,11 +14,13 @@ dotenv.config()
 const api = async () => {
     await dbConection()
     
+    const __filename = fileURLToPath(import.meta.url) 
+    const __dirname = path.dirname(__filename)
     
     server.use(cors());
       
     server.use(express.json())
-    server.use('/public', express.static(`/temp/imgs`))
+    server.use('/public', express.static(path.join(__dirname, '/temp/imgs')))
     server.use("/api/products", productsRoutes)
     
     server.listen(process.env.PORT, () => console.log(`Servidor corriendo en el puerto ${process.env.PORT}`))
