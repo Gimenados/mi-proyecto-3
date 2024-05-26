32b960f69a2c8af71965c9b4f810aa435e676547
gimenados <gimenasande1@gmail.com>
2024-05-26 13:48:39 -0300

    ADD endpoint edit y delete

--- store-api/src/controllers/productsController.js
+++ store-api/src/controllers/productsController.js
@@ -78,11 +78,11 @@ export const getProducts = async (req, res) => {
     const { search } = req.query;
 
     try {
-
         const searchBy = search ? { name: new RegExp(search, "i") } : {}
 
+        //                                                            Que no sea null ni undefined
         const products = await Products.find({...searchBy, deletedAt: {$in: [null, undefined]}}) //"i" nos ahorramos errores colaterales con las minusculas y mayusculas 
-        //Nuestra data del products
+        //Respuesta data del products
         res.json({
             products
         })
@@ -93,5 +93,112 @@ export const getProducts = async (req, res) => {
             ok: false,
             msg: "Ha habido un error con el servidor"
         })  //Error interno
+    }
+}
+
+//PARA EDITAR UN PRODUCTO
+export const editProduct = async (req, res) => {
+    const {id} = req.params
+    const {body, file} = req
+    try {
+        //Buscar el producto en la base de datos
+        const product = await Products.findById(id)
+
+        //Verificar si el producto existe o si ha sido eliminado
+        if (!product || product.deletedAt) {
+            return res.status(404)
+                .json({
+                    ok: false,
+                    msg: "Producto no encontrado o id inválido"
+                })
+        }
+
+        //Si se ha subido un archivo
+        let imageUrl = product.imgUrl
+
+        if (file) {
+            //Se lee el archivo de imagen desde un directorio temporal
+            const imageBuffer = fs.readFileSync(`./temp/imgs/${file.filename}`)
+        
+            //Se guarda la imagen en la base de datos creando un nuevo documento en el coleccion Images
+            const image = await Images.create({
+                fileName: file.filename,
+                img: {
+                    data: imageBuffer,
+                    contentType: "image/png"
+                }
+            })
+            
+            //Si no se puede guardar la imagen
+            if (!image) {
+                return res.status(400)
+                    .json({
+                        ok: false,
+                        msg: "No se pudo guardar correctamente la imagen."
+                    })
+            }
+
+            //Se elimina el archivo temporal
+            fs.rm(`./temp/imgs/${file.filename}`, error => {
+                    if (error) {
+                        console.log("Lo sentimos, no hemos podido eliminar el archivo")
+                    }
+                    console.log("El archivo se ha eliminado correctamente")
+                })
+
+            //Se actualiza la url de la imagen
+            imageUrl =`${process.env.BASE_URL}/images/${image._id}`
+        }
+
+        //Actualiza el producto en la base de datos
+        const productUpdated = await Products.findByIdAndUpdate(id, {
+            ...body,
+            imgUrl: imageUrl 
+        }, { new: true });
+
+        //Respuesta
+        res.json({
+            ok: true,
+            product: productUpdated,
+            msg: "El producto se actualizò correctamente."
+        })
+    //Manejo de errores
+    } catch (error) {
+        console.log("Ha habido un error al editar el producto.")
+        res.status(500)
+            .json({
+                ok: false,
+                msg: "Ha habido un error con el servidor"
+            })
+    }
+}
+
+//BORRAR UN PRODUCTO
+export const deleteProduct = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const product = await Products.findById(id);
+
+        if (!product || product.deletedAt) {
+            return res.status(404).json({
+                ok: false,
+                msg: "El producto no fue encontrado."
+            })
+        }
+
+        await Products.findByIdAndUpdate(id, { deletedAt: new Date() }, { new: true });
+
+        res.json({
+            ok: true,
+            msg: "Producto elemininado correctamente"
+        })
+
+    } catch (error) {
+        console.log("Ha habido un error al editar el producto.")
+        res.status(500)
+            .json({
+                ok: false,
+                msg: "Ha habido un error con el servidor"
+            })
     }
 }
--- store-api/src/routes/products.routes.js
+++ store-api/src/routes/products.routes.js
@@ -1,13 +1,15 @@
 import express from "express"
 import upload from "../libs/storage.js";
 
-import { createProduct, getProducts } from "../controllers/productsController.js";
+import { createProduct, deleteProduct, editProduct, getProducts } from "../controllers/productsController.js";
 
 const route = express.Router()
 
 route
     .post("/", upload.single("image"), createProduct) //Con single decimos que se suba una sola imagen
     .get("/", getProducts)
+    .put("/edit/:id", upload.single("image"), editProduct)
+    .delete("/delete/:id", deleteProduct)
 
 export default route;
 
